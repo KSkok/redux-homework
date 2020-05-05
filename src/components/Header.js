74e06657628b625782ef9fc8c9c19e20160df913
Katerina Skok <k.skok@sana-commerce.com>
2020-05-05 14:21:21 +0300

    used controlled select instead of uncontrolled one

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ReduxContext } from "./redux/ReduxContext";
 import { newLanguageId } from "../behavior/actions";
 import { LANGUAGES } from "../constants/languages";
@@ -10,14 +10,16 @@ const Header = () => {
     dispatch,
   } = reduxContext;
 
+  const [language, setLanguage] = useState(languageId);
   const handleChange = (e) => {
+    setLanguage(e.target.value);
     dispatch(newLanguageId(e.target.value));
   };
 
   return (
     <>
       <label htmlFor="language">Language: </label>
-      <select id="language" defaultValue={languageId} onChange={handleChange}>
+      <select id="language" value={language} onChange={handleChange}>
         <option value={LANGUAGES.EN}>EN</option>
         <option value={LANGUAGES.UA}>UA</option>
       </select>
